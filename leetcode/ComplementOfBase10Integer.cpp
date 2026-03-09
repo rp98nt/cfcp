e8b84896443dc92df3f2c76a6bd9c215a1b60dc0
rp98nt <roshanpatra@zohomail.in>
2026-03-09 20:05:20 +0530

    Update ComplementOfBase10Integer.cpp

--- leetcode/ComplementOfBase10Integer.cpp
+++ leetcode/ComplementOfBase10Integer.cpp
@@ -3,32 +3,66 @@
 #include<stack>
 using namespace std;
 
+// int main(){
+//     int n;
+//     cin>>n;
+//     stack<int> s;
+//     while(n!=0){
+//         int temp = n%2;
+//         s.push(temp);
+//         n = n/2;
+//     }
+//     stack<int> q;
+//     while(s.size()!=0){
+//         int temp = s.top();
+//         s.pop();
+//         if(temp == 1)
+//             q.push(0);
+//         else if(temp == 0)
+//             q.push(1);
+//     }
+//     int temp = 0, x = 0;
+//     while(q.size()!=0){
+//         int flam = q.top();
+//         if(flam)
+//             temp += pow(2,x);
+//         x++;
+//         q.pop();
+//     }
+//     cout<<temp;
+//     return 0;
+// }
+
+// int main(){
+//     int n;
+//     cin>>n;
+//     int m = n;
+//     int mask = 0;
+//     if(n == 0){
+//         cout<<1;
+//     } else {
+//         while(m!=0){
+//             mask = (mask<<1) | 1;
+//             m = m>>1;
+//         }
+//         int ans = (~n) & mask;
+//         cout<<ans;
+//     }
+//     return 0;
+// }
+
 int main(){
     int n;
     cin>>n;
-    stack<int> s;
-    while(n!=0){
-        int temp = n%2;
-        s.push(temp);
-        n = n/2;
+    int mask = 0, m = n;
+    if(n == 0)
+        cout<<1;
+    else{
+        while(m!=0){
+            mask = (mask<<1) | 1;
+            m = m>>1;
+        }
+        cout<<((~n) & mask);
     }
-    stack<int> q;
-    while(s.size()!=0){
-        int temp = s.top();
-        s.pop();
-        if(temp == 1)
-            q.push(0);
-        else if(temp == 0)
-            q.push(1);
-    }
-    int temp = 0, x = 0;
-    while(q.size()!=0){
-        int flam = q.top();
-        if(flam)
-            temp += pow(2,x);
-        x++;
-        q.pop();
-    }
-    cout<<temp;
     return 0;
 }
