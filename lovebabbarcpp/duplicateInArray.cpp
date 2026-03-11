3b621f00323cbc39a009ecab6b04a581be10f95f
rp98nt <roshanpatra@zohomail.in>
2026-03-11 20:53:23 +0530

    Update duplicateInArray.cpp

--- lovebabbarcpp/duplicateInArray.cpp
+++ lovebabbarcpp/duplicateInArray.cpp
@@ -1,17 +1,30 @@
+/*
+    You are given an array of size N containing each number between 1 and N-1 atleast once. There is a single integer value that is present in the array twice. Find it.
+*/
+
 #include<iostream>
 #include<vector>
 #include<map>
 using namespace std;
 
+// int duplicateInArray(vector<int> v){
+//     map<int, int> m;
+//     for(int i=0; i<v.size(); i++){
+//         m[v[i]]++;
+//     }
+//     for(auto x:m)
+//         if(x.second == 2)
+//             return x.first;
+//     return -1;
+// }
+
 int duplicateInArray(vector<int> v){
-    map<int, int> m;
-    for(int i=0; i<v.size(); i++){
-        m[v[i]]++;
-    }
-    for(auto x:m)
-        if(x.second == 2)
-            return x.first;
-    return -1;
+    int ans = 0;
+    for(auto x:v)
+        ans = ans ^ x;
+    for(int i=1; i<v.size(); i++)
+        ans = ans ^ i;
+    return ans;
 }
 
 int main(){
