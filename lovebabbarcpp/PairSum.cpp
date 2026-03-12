e3f425860c19b8c8ef92c042cfaf0e0bdfc77827
rp98nt <roshanpatra@zohomail.in>
2026-03-12 23:00:07 +0530

    Update PairSum.cpp

---
+++ lovebabbarcpp/PairSum.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+vector<vector<int>> pairSum(vector<int> &arr, int s){
+    vector<vector<int>> ans;
+    for(int i=0; i<arr.size(); i++){
+        for(int j=i+1; j<arr.size(); j++){
+            if(arr[i]+arr[j] == s){
+                vector<int> temp;
+                temp.push_back(min(arr[i], arr[j]));
+                temp.push_back(max(arr[i], arr[j]));
+                ans.push_back(temp);
+            }
+        }
+    }
+    return ans;
+}
+
+int main(){
+    int n;
+    cin>>n;
+    vector<int> v;
+    while(n--){
+        int temp;
+        cin>>temp;
+        v.push_back(temp);
+    }
+    int s;
+    cin>>s;
+    vector<vector<int>> result = pairSum(v, s);
+    return 0;
+}
